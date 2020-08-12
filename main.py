a90a9dceed1d593f2da2a7aa4f285085d1f42ac0
Bhupinder Singh <bsinghr1998@gmail.com>
2020-08-12 02:36:45 +0000

    started set up for game

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-print("Hello Python")
+'''
+Text adventure game 
+'''
+print("Welcome to Aventura!\n")
+print("Prepare for the journey...")
+health = 10
+infected = False
 
- 
+name = input("What is your name? \n")
+
+print("\n" + name + "? Not that it matters now, prisoner 147\n")
+
+age = input("What is your age? \n")
+
+print("Test: Hello", name, "you are", age, "years old\n")
