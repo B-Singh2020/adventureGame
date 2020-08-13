2de5a8fa90db0b38781297aed30ca30c062dfe0d
Bhupinder Singh <bsinghr1998@gmail.com>
2020-08-13 00:47:04 +0000

    added in start of game dialogue

--- main.py
+++ main.py
@@ -6,10 +6,21 @@ print("Prepare for the journey...")
 health = 10
 infected = False
 
-name = input("What is your name? \n")
+name = input("What is your name? ")
 
 print("\n" + name + "? Not that it matters now, prisoner 147\n")
 
-age = input("What is your age? \n")
+age = int(input("What is your age? "))
 
-print("Test: Hello", name, "you are", age, "years old\n")
+#print("Test: Hello", name, "you are", age, "years old\n")
+
+ageCheck = age - 18
+if(ageCheck < 0):
+    print("You are not old enough for this adventure!\n")
+    
+else:
+    print("You are ready for this adventure!\n")
+    wants_to_play = input("Are you ready to begin your journey? (yes/no) ")
+    if(wants_to_play == "yes"):
+        print("Let us begin!!\n")
+
